5d8a00be2a784d6bc4c581e0a52149aa24c33d5b
Seyi Ayo <oluwaseyiayoola97@gmail.com>
2025-01-02 22:47:28 +0100

    wsgi fix

--- shoestore/wsgi.py
+++ shoestore/wsgi.py
@@ -1,10 +1,10 @@
 """
 WSGI config for shoestore project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named ``app``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
@@ -13,4 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shoestore.settings')
 
-application = get_wsgi_application()
+# This is the WSGI application variable that Vercel looks for
+app = application = get_wsgi_application()
